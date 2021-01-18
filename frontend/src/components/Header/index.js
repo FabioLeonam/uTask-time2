e9d7b30485317d7a70e91129ffc8dbdf05277955
Fábio L <f.leonam@hotmail.com>
2021-01-18 16:02:20 -0300

    Renderização do componente Header

---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './styles.css';
+import plus from '../../assets/plus.svg';
+
+const Header = () => {
+
+  return(
+    <> {/* Fragment */}
+      <header className="header">
+        <h1 className="u-task">uTask</h1>
+
+        <form className="inputTask">
+          <input type="text" className="todoInput" placeholder="Adicione uma tarefa"/>
+          <button className="submitButton" type="submit">
+            <img  src={plus} alt="Adicionar tarefa" />
+          </button>
+        </form>
+      </header>
+    </>
+  )
+}
+
+export default Header;
