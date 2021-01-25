799c0b8fe63d1e8929e13edd17aaa38c5e7cd270
Fábio L <f.leonam@hotmail.com>
2021-01-25 11:14:59 -0300

    Arquivos do componente TodoList passados para page Home e conexão do frontend com o backend através do axios

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import Header from '../../components/Header';
-import TodoList from '../../components/TodoList';
+import Task from '../../components/Task';
+import beach from '../../assets/beach.svg';
+import './style.css';
 
 const Home = () => {
   
   return(
     <>
       <Header />
-      <TodoList />
+    
+      <div className="todo-container">
+
+        <span className="titleBox">TODO</span>
+        <div className="taskToDo">
+          <Task />
+        </div>
+        <div className="empty">
+          <img src={beach} alt="Sem tarefas"/>          
+          <span>Nada por aqui!</span>
+        </div>
+
+        <span className="titleBox">DONE</span>
+        {/* <div className="taskDone">Aqui vem as Tasks feitas</div> */}
+        <div className="empty">
+          <img src={beach} alt="Sem tarefas"/>          
+          <span>Nada por aqui!</span>
+        </div>
+
+      </div>
     </>
   )
 
