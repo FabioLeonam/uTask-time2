33b7df4db1e3a8e8a553dcd9fc21ca631f9ea067
Fábio L <f.leonam@hotmail.com>
2021-01-26 09:43:03 -0300

    add funcionalidades do submit, handleDone e handleDelete.

--- frontend/src/components/Task/index.js
+++ frontend/src/components/Task/index.js
@@ -1,21 +1,30 @@
 import React from 'react';
+import api from '../../services/api';
 import './style.css';
 import doneIcon from '../../assets/done.svg';
 import deleteIcon from '../../assets/deleteIcon.svg';
 
 
+const Task = ({id, inputText}) => {
+  
+  function handleDone(){
+    api.put(`/tasks/${id}`, {taskStatus: true});
+  }
+
+  function handleDelete(){
+    api.delete(`/tasks/${id}`)
+  }
 
 
-const Task = () => {
   return(
     <div className="task">
-      <p>Comprar leite no mercado</p>
+      <p>{inputText}</p>
       <div className ="buttons">
-        <button className="doneButton">
+        <button className="doneButton" onClick={handleDone}>
           <img src={doneIcon} alt="Tarefa feita"/>
         </button>
-        <button className="deleteButton">
-          <img src={deleteIcon} alt="Tarefa feita"/>
+        <button className="deleteButton" onClick={handleDelete}>
+          <img src={deleteIcon} alt="Tarefa feita" />
         </button>
       </div>
       
